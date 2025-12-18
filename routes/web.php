bdf6ce14e38bcbf22c2aa6ddb434107d5be043da
FaizKhairann <faizkhairan6@gmail.com>
2025-12-18 23:33:27 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/dashboard', function () {
-    return redirect('/dashboard/products');
-})->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::prefix('dashboard')->group(function () {
     Route::resource('products', ProductController::class);
