83ece52b11433465ab8c44aeb6c5e3a68f631256
emmirfahrezi <emmir.fahrezi1@gmail.com>
2025-12-18 16:05:08 +0700

    kesalahan

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,24 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
+})->name('logout');
+
+Route::resource('products', ProductController::class);
