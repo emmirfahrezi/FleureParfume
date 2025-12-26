50d16009e2b62311e4ad13bf1896e08ebdc561ac
FaizKhairann <faizkhairan6@gmail.com>
2025-12-26 16:57:02 +0700

    Feat(belum) : auth login google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Login - Fleure Parfume</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
@@ -20,7 +21,7 @@
         <p class="text-center mb-6" style="color:#5a4a30;">Welcome, please sign in</p>
 
         @if(session('error'))
-            <p class="text-red-500 text-sm mb-3">{{ session('error') }}</p>
+        <p class="text-red-500 text-sm mb-3">{{ session('error') }}</p>
         @endif
 
         <form method="POST" action="/login">
@@ -38,14 +39,14 @@
 
             <div class="text-right mb-4">
                 <a href="/forgot-password"
-                class="text-sm"
-                style="color:#4B2E15; text-decoration: underline;">
+                    class="text-sm"
+                    style="color:#4B2E15; text-decoration: underline;">
                     Forgot your password?
                 </a>
             </div>
 
             <button class="w-full py-2 rounded-xl font-semibold text-white"
-                    style="background-color:#4B2E15;">
+                style="background-color:#4B2E15;">
                 Sign In
             </button>
 
@@ -54,7 +55,17 @@
                 <a href="/register" class="font-semibold" style="color:#4B2E15;">Daftar</a>
             </p>
         </form>
+
+        <!-- GOOGLE LOGIN -->
+        <div class="mt-6">
+            <a href="{{ route('google.login') }}"
+                class="w-full flex items-center justify-center gap-2 border px-4 py-2 rounded-xl hover:bg-gray-100">
+                <img src="https://developers.google.com/identity/images/g-logo.png" width="18">
+                Login dengan Google
+            </a>
+        </div>
     </div>
 
 </body>
+
 </html>
